ce13f2bc7867fe26bc76faa53722dc0a6e5b5ce6
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2020-08-09 14:44:10 +0200

    Prepare for 0.02 version

--- End.pm
+++ End.pm
@@ -6,7 +6,7 @@ use warnings;
 use Class::Utils qw(set_params);
 use Error::Pure qw(err);
 
-our $VERSION = 0.01;
+our $VERSION = 0.02;
 
 # Constructor.
 sub new {
@@ -196,6 +196,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.01
+0.02
 
 =cut
