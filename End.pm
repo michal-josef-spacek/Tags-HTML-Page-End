87084120c88ecc852a80f31a76ba8d5ad8322b0b
Michal Josef Špaček <michal.josef.spacek@gmail.com>
2024-05-23 13:22:33 +0200

    Prepare for 0.07 version

--- End.pm
+++ End.pm
@@ -4,7 +4,7 @@ use base qw(Tags::HTML);
 use strict;
 use warnings;
 
-our $VERSION = 0.06;
+our $VERSION = 0.07;
 
 # Constructor.
 sub new {
@@ -191,6 +191,6 @@ BSD 2-Clause License
 
 =head1 VERSION
 
-0.06
+0.07
 
 =cut
